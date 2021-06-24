c65b1f1102490cc8ced30e5e9c395395c647fcfe
Zachary Robinson <zachary_robinson@icloud.com>
2021-06-24 13:20:27 -0400

    Dark mode applies to canvas

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ function $(id) {
 let c = $("canvas").getContext("2d");
 let width = c.canvas.width;
 let height = c.canvas.height;
+let darkMode;
+let data = {};
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	darkMode = true;
+} else {
+	darkMode = false;
+}
+
 drawBackground();
 
 function drawBackground() {
-	c.strokeStyle = "black";
-	c.fillStyle = "white";
+	c.strokeStyle = darkMode ? "white" : "black";
+	c.fillStyle = darkMode ? "black" : "white";
 	// draw white background
 	c.beginPath();
 	c.rect(1, 1, width - 2, height - 2);
@@ -21,7 +30,14 @@ function drawBackground() {
 	c.stroke();
 }
 
-$("simulate").addEventListener("click", function () {
+$("simulate").addEventListener("click", simulate);
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+	darkMode = e.matches ? true : false;
+	simulate();
+});
+
+function simulate() {
 	const darts = $("points").value;
 	const origin = { x: 0.5, y: 0.5 };
 	let points = [];
@@ -41,7 +57,7 @@ $("simulate").addEventListener("click", function () {
 			c.fillStyle = "hsl(217, 71%, 53%)";
 		} else {
 			point.inCircle = false;
-			c.fillStyle = "black";
+			c.fillStyle = darkMode ? "white" : "black";
 		}
 
 		if (points.length < width * 20) {
@@ -58,11 +74,38 @@ $("simulate").addEventListener("click", function () {
 	}
 
 	let estimation = 4 * (totalInCircle / darts);
+	let difference = Math.abs(Math.PI - estimation);
 
 	let output = "";
 
 	output += `<p>Estimated Value of PI: ${estimation}</p>`;
-	output += `<p>Difference: ${Math.abs(Math.PI-estimation)}</p>`;
+	output += `<p>Difference: ${difference}</p>`;
 
 	$("info").innerHTML = output;
-});
+
+	data = {
+		totalPoints: darts,
+		totalPointsInCircle: totalInCircle,
+		estimatedValueOfPI: estimation,
+		estimatedValueDifference: difference,
+		points: points,
+	};
+}
+
+// Ended up being too big of a file usually, around
+// 30 MB, which crashes Notepad when you try to open
+// it and sometimes crashes the site itself. Cool
+// idea, but didn't execute well. I could always use
+// the PasteBin API to host the file, but its more
+// work than it's worth. Maybe another time...
+
+/* $("copy").addEventListener("click", function () {
+	let e = document.createElement("a");
+	e.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(JSON.stringify(data)));
+	console.log(JSON.stringify(data))
+	e.setAttribute("download", "simulationData.json");
+	e.style.display = "none";
+	document.body.appendChild(e);
+	e.click();
+	document.body.removeChild(e);
+}); */
